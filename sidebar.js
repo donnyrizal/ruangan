df2deb4ac4be3e6f2d046bb996ad55bc12d215a3
donny <donnyrizaladhip@rocketmail.coom>
2026-01-27 15:06:30 +0700

    Updates

--- sidebar.js
+++ sidebar.js
@@ -9,7 +9,7 @@ const sidebarHTML = `
     <div class="p-6 flex items-center justify-between gap-3">
         <div class="flex items-center gap-3">
         <a
-            href="./"
+            href="https://donnyrizal.github.io/ruangan/"
             class="flex items-center transition-opacity hover:opacity-90"
           >
             <svg
